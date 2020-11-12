9a4449b0eeb76dad30c8468dd7ea3b0e2716a805
Kirill <pn79122821224@gmail.com>
2020-11-12 16:10:22 +0500

    fix(findPathToFile): fixes lint issues

--- src/findPathToFile.ts
+++ src/findPathToFile.ts
@@ -1,26 +1,31 @@
-import path from 'path';
-import fs from 'fs';
+import fs from "fs";
+import path from "path";
 
-
-type FindPathToFile = (fileName: string, options?: { cwd?: string }) => Promise<{ filePath: string; dirPath: string; } | Error>
-export const findPathToFile: FindPathToFile = async (fileName, options = {}) => {
-  const initialDirectoryPath = options.cwd || '';
+type FindPathToFile = (
+  fileName: string,
+  options?: { cwd?: string }
+) => Promise<{ filePath: string; dirPath: string } | Error>;
+export const findPathToFile: FindPathToFile = async (
+  fileName,
+  options = {}
+) => {
+  const initialDirectoryPath = options.cwd || "";
 
   let pathToFile = path.resolve(initialDirectoryPath, fileName);
 
-  while(true) {
+  while (true) {
     if (fs.existsSync(pathToFile)) {
       return {
         filePath: pathToFile,
-        dirPath: path.dirname(pathToFile),
+        dirPath: path.dirname(pathToFile)
       };
     }
 
     const pathToDir = path.dirname(pathToFile);
-    if (pathToDir === '/') {
-      return Promise.reject(new Error('There is no such file.'))
+    if (pathToDir === "/") {
+      return Promise.reject(new Error("There is no such file."));
     }
 
-    pathToFile = path.resolve(pathToFile, '../..', fileName);
-  } 
-}
+    pathToFile = path.resolve(pathToFile, "../..", fileName);
+  }
+};
